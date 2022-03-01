1b2b82622ccef29fce0f80cb2c32cf57e05fefea
AVINASH SUBRAMANIAN <avsubram@mtu.edu>
2022-03-01 08:31:14 -0600

    Create header.vue

---
+++ TrafficLight/src/components/header.vue
@@ -0,0 +1,33 @@
+<!-- Begin Header Component -->
+<template>
+    <div>
+        <div class="newsNowLogoDiv">
+            <img class="newsNowLogo" src="@/assets/horizontalLogo.png" />
+        </div>
+        <!-- headerTitle retreived from the props value sent in from the App.vue parent component -->
+        <div class="serverStatusHeaderDiv">
+            <h2 class="serverStatusHeader">{{ headerTitle }}</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: "LogoHeader",
+      props: {
+        headerTitle: String
+      },
+    };
+    </script>    
+
+<style scoped>
+.newsNowLogoDiv {
+  margin: 10px auto;
+  width: 90%;
+  max-width: 250px;
+}
+.newsNowLogoDiv .newsNowLogo {
+  width: 100%;
+  height: auto;
+}
+</style>
